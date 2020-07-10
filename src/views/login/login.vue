f866f52cafa1b06c79f860be1e141da66dde89c9
LBB <3291282554@qq.com>
2020-07-10 13:35:29 +0800

    睡觉

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -7,13 +7,43 @@
         <span class="wire"></span>
         <span class="fun_name">用户登录</span>
       </div>
+      <el-input
+        placeholder="请输入手机号"
+        v-model="number"
+        style="width: 394px;height:45px;margin:28px 41px 0px 43px"
+      >
+        <i slot="prefix" class="el-input__icon el-icon-user"></i>
+      </el-input>
+      <el-input
+        placeholder="请输入密码"
+        v-model="password"
+        style="width: 394px;height:45px;margin:25px 41px 0px 43px;"
+        :show-password="true"
+      >
+        <i slot="prefix" class="el-input__icon el-icon-lock"></i>
+      </el-input>
+      <el-input
+        placeholder="请输入验证码"
+        v-model="auth_code"
+        style="width: 284px;height:44px;margin:25px 0px 0px 42px;"
+      >
+        <i slot="prefix" class="el-input__icon el-icon-key"></i>
+      </el-input>
     </div>
     <img src="@/assets/img/login_backimg.png" alt />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      number: "",
+      password: "",
+      auth_code: ""
+    };
+  }
+};
 </script>
 
 <style lang='less'>
@@ -21,7 +51,7 @@ export default {};
   display: flex;
   width: 100%;
   height: 100%;
-  background: linear-gradient(225deg, #1493fa, #01c6fa);
+  background: linear-gradient(to right, #1493fa, #01c6fa);
   align-items: center;
   justify-content: space-around;
   .left {
@@ -29,6 +59,7 @@ export default {};
     height: 550px;
     background: #f5f5f5;
     .title {
+      display: flex;
       width: 246px;
       height: 28px;
       margin: 44px 184px 0 48px;
@@ -56,8 +87,13 @@ export default {};
         font-weight: 400;
         text-align: left;
         color: #0c0c0c;
-        margin-left: 15px;
+        margin-left: 14px;
       }
+    }
+    .el-input__inner {
+      background: #ffffff;
+      height: 100%;
+      font-size: 13px;
     }
   }
 }
