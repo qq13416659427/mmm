52d385dee9fab16fd094da571528a48c89347dd1
LBB <3291282554@qq.com>
2020-07-10 10:33:20 +0800

    引入less

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <router-view></router-view>
+  <div class="app">
+    <span>123</span>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang='less'>
+.app {
+  background-color: violet;
+  span {
+    color: aqua;
+  }
+}
 </style>
